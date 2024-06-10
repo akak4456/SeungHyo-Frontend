e3bc4142548ea8e6425b4b9215d88d5efe30410b
Ella Eom <eomji1233@gmail.com>
2024-06-10 16:47:04 +0900

    [ADD] 기본적인 index, header 구조 추가함
    1. Root component 를 생성하여서 index 화면을 구성하도록 함
    2. Header component 를 생성하여서 화면들의 공통 header 가 만들어지도록
       조치함
    3. responsive.js 를 만들어서 훗날 mobile 에도 대응가능하도록 함

---
+++ src/header/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './header.css';
+import {Mobile, PC} from '../responsive.js';
+
+export default class Header extends React.Component {
+    render() {
+        // TODO 나중에 모바일 및 반응형 컴포넌트 제작하기
+        return (
+            <div>
+                <Mobile><p>Header Mobile</p></Mobile>
+                <PC><p>Header PC</p></PC> 
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './reset.css';
 import './index.css';
+import Header from './header/header.js';
+
+class Root extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <Header />
+        )
+    }
+}
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Root />);
