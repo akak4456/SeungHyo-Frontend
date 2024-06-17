cf4dbb629d55a9a2dd1631cede0dc090dd2e947a
akak4456 <akak4456@naver.com>
2024-06-17 20:40:35 +0900

    [CNG] user statistics left 사이드 전체적인 형태 구성함

--- src/main/user/user.js
+++ src/main/user/user.js
@@ -16,10 +16,28 @@ const User = () => {
             <main className={styles.UserRoot}>
                 <p className={styles.UserName}>akak4456</p>
                 <blockquote className={styles.UserMessage}>컴공 17 조승효</blockquote>
-                <div className={styles.UserDiv}></div>
+                <div className={styles.UserDivTop}></div>
                 <div className={classNames(styles.UserStatisticsRoot, {[styles.UserStatisticsRootMobile]: isMobile})}>
-                    <div>
-
+                    <div className={classNames(styles.UserStatisticsLeft, {[styles.UserStatisticsLeftMobile]: isMobile})}>
+                        <div className={styles.UserDiv}></div>
+                        <div className={styles.UserStatisticsLeftRow}>
+                            <p className={classNames(styles.UserStatisticsLeftTitle)}>등수</p>
+                            <p className={styles.UserStatisticsLeftContent}>4579</p>
+                        </div>
+                        <div className={styles.UserDiv}></div>
+                        <div className={styles.UserStatisticsLeftRankGraph}></div>
+                        <div className={classNames(styles.UserDiv, styles.UserDivMarginTop24)}></div>
+                        <div className={styles.UserStatisticsLeftRow}>
+                            <p className={classNames(styles.UserStatisticsLeftTitle, styles.UserStatisticsLeftTitleCorrect)}>맞았습니다</p>
+                            <p className={styles.UserStatisticsLeftContent}>4579</p>
+                        </div>
+                        <div className={styles.UserDiv}></div>
+                        <div className={styles.UserStatisticsLeftRow}>
+                            <p className={classNames(styles.UserStatisticsLeftTitle, styles.UserStatisticsLeftTitleWrong)}>틀렸습니다</p>
+                            <p className={styles.UserStatisticsLeftContent}>4579</p>
+                        </div>
+                        <div className={styles.UserDiv}></div>
+                        <div className={styles.UserStatisticsLeftRatioGraph}></div>
                     </div>
                     <div>
 
