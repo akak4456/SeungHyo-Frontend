176130044e293e3375a678d23b44cb304e7f04c4
akak4456 <akak4456@naver.com>
2024-07-10 16:06:57 +0900

    [CNG] CheckBox components 리팩토링함

---
+++ src/components/check-box.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+
+/**
+ * 체크박스 컴포넌트를 나타냅니다.
+ * @param {Props} props - input tag 에 들어갈 수 있는 prop 지정 가능함
+ * @param {String} props.id - input tag 의 id
+ * @param {String} props.text - 체크박스를 표시할 때 들어가는 text
+ * @returns {JSX.Element} 사용자 이름을 출력하는 JSX 요소
+ */
+const CheckBox = (props) => {
+	const StyledDiv = styled.div`
+		display: flex;
+		align-items: center;
+		& label {
+			margin-left: 8px;
+		}
+		& input {
+			width: 16px;
+			height: 16px;
+		}
+	`;
+	return (
+		<StyledDiv>
+			<input type="checkbox" {...props} />
+			<label for={props.id}>{props.text}</label>
+		</StyledDiv>
+	);
+};
+
+export default CheckBox;
