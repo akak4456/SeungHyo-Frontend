e8b38f892c8ec6e2acdc1fc6a1aca358d1c0fd46
DESKTOP-2UK7O3T\jo <akak4456@naver.com>
2024-07-16 15:02:00 +0900

    [CNG] responsive 에 주석 추가함

--- src/responsive.js
+++ src/responsive.js
@@ -1,3 +1,9 @@
+/*
+responsive.js 는
+hooks/media-query.js 에 의해 삭제될 예정임
+기존에 사용하던 코드는 대체하도록 하고
+이 파일은 삭제할 것
+*/
 import React from 'react';
 import { useMediaQuery } from 'react-responsive';
 export const isTabletQuery = '(max-width:961px)';
