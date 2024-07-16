73cdb4b659a637cb7fc794cec2a43efda6987303
DESKTOP-2UK7O3T\jo <akak4456@naver.com>
2024-07-16 16:46:47 +0900

    [CNG] footer mobile 에서 다소 이상하게 보이는 현상 수정함

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import Logo from '../components/logo';
 import styled from 'styled-components';
+import { useIsMobile } from '../hooks/media-query.js';
 const StyledFooter = styled.footer`
 	padding-left: 12.5%;
 	padding-right: 12.5%;
 	display: flex;
-	align-items: flex-end;
+	align-items: ${(props) => (props.isMobile ? 'center' : 'flex-end')};
 	height: 200px;
 	padding-bottom: 32px;
+	text-align: center;
+	flex-direction: ${(props) => (props.isMobile ? 'column' : 'row')};
+	justify-content: flex-end;
 `;
 const StyledSpan = styled.span`
 	color: var(--color-normal-text-color);
+	margin-bottom: ${(props) => (props.isMobile ? '8px' : '0')};
 `;
 const Footer = (props) => {
+	const isMobile = useIsMobile();
 	return (
-		<StyledFooter>
-			<StyledSpan>
+		<StyledFooter isMobile={isMobile}>
+			<StyledSpan isMobile={isMobile}>
 				Copyright © 2024 주식회사 승효. All rights reserved.
 			</StyledSpan>
-			<Logo marginLeft={'auto'} />
+			<Logo marginLeft={isMobile ? '' : 'auto'} />
 		</StyledFooter>
 	);
 };
