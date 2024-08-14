e4e67b1918d0511f42ab31c536db2f6e5de17146
adele <akak4456@naver.com>
2024-08-14 12:32:39 +0900

    [ADD] 댓글 리스트 api 엮음

--- src/api/Board.js
+++ src/api/Board.js
@@ -43,3 +43,25 @@ export const getBoardOne = (boardNo, onSuccess) => {
 			console.log(exception);
 		});
 };
+
+export const getReplyList = (boardNo, page, size, onSuccess) => {
+	commonAPI
+		.get(
+			`/api/v1/reply/${boardNo}?page=${page}&size=${size}`,
+			{},
+			{
+				withCredentials: true,
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			}
+		)
+		.then((response) => {
+			if (response.data.code === '0') {
+				onSuccess(response.data.data);
+			}
+		})
+		.catch((exception) => {
+			console.log(exception);
+		});
+};
--- src/pages/article.js
+++ src/pages/article.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { NavLink, useLocation } from 'react-router-dom';
+import { NavLink, useLocation, useSearchParams } from 'react-router-dom';
 import { HandThumbsUp } from 'react-bootstrap-icons';
 import NormalEditor from '../components/editor-normal';
 import SourceEditor from '../components/editor-source';
 import Dropdown from '../components/dropdown';
 import NormalButton from '../components/button-normal';
-import { getBoardOne } from '../api/Board';
+import { getBoardOne, getReplyList } from '../api/Board';
 import { timeAgo } from '../util';
+import Pagination from '../components/pagination';
 const ReplyAddRootDiv = styled.div`
 	display: flex;
 	width: 100%;
@@ -81,21 +82,21 @@ const ReplyContentRootDiv = styled.div`
 		color: var(--color-normal-text-color);
 	}
 `;
-const Reply = (props) => {
+const Reply = ({ replyNo, author, date, recommendCount, content }) => {
 	return (
 		<ReplyRootDiv>
 			<ReplyTitleDiv>
-				<span>{props.author}</span>
-				<span style={{ marginLeft: '16px' }}>{props.date}</span>
+				<span>{author}</span>
+				<span style={{ marginLeft: '16px' }}>{date}</span>
 				<HandThumbsUp
 					style={{ marginLeft: '16px' }}
 					size={'16px'}
 				></HandThumbsUp>
-				<span style={{ marginLeft: '8px' }}>{props.recommendCount}</span>
+				<span style={{ marginLeft: '8px' }}>{recommendCount}</span>
 				<ReplyLike>좋아요</ReplyLike>
 			</ReplyTitleDiv>
 			<ReplyContentRootDiv>
-				<p>{props.content}</p>
+				<p>{content}</p>
 			</ReplyContentRootDiv>
 		</ReplyRootDiv>
 	);
@@ -117,6 +118,21 @@ const ArticleLinkRootDiv = styled.div`
 		font-size: 28px;
 	}
 `;
+
+const PaginationRootDiv = styled.div`
+	width: 100%;
+	margin-top: 24px;
+	display: flex;
+	& div {
+		margin: 0 auto;
+	}
+`;
+const ReplyTitle = styled.p`
+	margin-top: 16px;
+	font-size: 25px;
+	font-weight: bold;
+	color: var(--color-normal-text-color);
+`;
 const Article = (props) => {
 	const location = useLocation();
 	// 경로를 '/'로 분할
@@ -124,40 +140,99 @@ const Article = (props) => {
 
 	// 숫자 ID는 세 번째 부분에 위치
 	const boardNo = parts[2];
-	const [boardData, setBoardData] = useState();
+
+	const prevLink = () => {
+		searchParams.set('page', startPage - 1 - 1);
+		searchParams.set('size', 10);
+		setSearchParams(searchParams);
+	};
+	const nextLink = () => {
+		searchParams.set('page', endPage + 1 - 1);
+		searchParams.set('size', 10);
+		setSearchParams(searchParams);
+	};
+	const goToLink = (num) => {
+		searchParams.set('page', num - 1);
+		searchParams.set('size', 10);
+		setSearchParams(searchParams);
+	};
+	const [searchParams, setSearchParams] = useSearchParams();
+	const page = parseInt(searchParams.get('page')) || 0;
+	const size = parseInt(searchParams.get('size')) || 10;
+	const [pageData, setPageData] = useState();
+	const startPage = Math.floor(page / size) * size + 1;
+	let endPage = startPage + size - 1;
+	if (pageData && endPage > pageData.totalPages) {
+		endPage = pageData.totalPages;
+	}
+
 	useEffect(() => {
 		getBoardOne(boardNo, (data) => {
 			console.log(data);
-			setBoardData(data);
+			setPageData((state) => ({
+				...state,
+				board: data,
+			}));
 		});
-	}, []);
+		getReplyList(boardNo, page, size, (data) => {
+			console.log(data);
+			setPageData((state) => ({
+				...state,
+				reply: data,
+			}));
+			window.scrollTo({
+				top: 0,
+				behavior: 'smooth',
+			});
+		});
+	}, [page, size]);
+
 	return (
 		<ArticleRootMain>
-			{boardData && (
+			{pageData && pageData.board && (
 				<>
 					<ArticleTitleDiv>
-						<p>{boardData.boardTitle}</p>
+						<p>{pageData.board.boardTitle}</p>
 					</ArticleTitleDiv>
 					<ArticleLinkRootDiv>
-						<NavLink to={`/problem/${boardData.problemNo}`}>
-							{boardData.problemNo}번 - {boardData.problemTitle}
+						<NavLink to={`/problem/${pageData.board.problemNo}`}>
+							{pageData.board.problemNo}번 - {pageData.board.problemTitle}
 						</NavLink>
 					</ArticleLinkRootDiv>
 					<Reply
-						author={boardData.boardMemberId}
-						date={timeAgo(boardData.boardRegDate)}
-						recommendCount={boardData.boardLikeCount}
-						content={boardData.boardContent}
+						author={pageData.board.boardMemberId}
+						date={timeAgo(pageData.board.boardRegDate)}
+						recommendCount={pageData.board.boardLikeCount}
+						content={pageData.board.boardContent}
 					/>
 				</>
 			)}
-
-			<Reply
-				author={'akak4478'}
-				date={'10분전'}
-				recommendCount={'10'}
-				content={'이건 댓글이에요 그리고 댓글은 페이징 처리가 되지요'}
-			/>
+			<ReplyTitle>댓글</ReplyTitle>
+			{pageData &&
+				pageData.reply &&
+				pageData.reply.content &&
+				pageData.reply.content.map((reply) => (
+					<Reply
+						key={reply.replyNo + 'reply'}
+						replyNo={reply.replyNo}
+						author={reply.memberId}
+						date={timeAgo(reply.regDate)}
+						recommendCount={reply.likeCount}
+						content={reply.replyContent}
+					/>
+				))}
+			<PaginationRootDiv>
+				<Pagination
+					prevLink={prevLink}
+					nextLink={nextLink}
+					minVal={startPage}
+					maxVal={endPage}
+					goToLink={goToLink}
+					isPrevInclude={startPage != 1}
+					isNextInclude={pageData && endPage != pageData.totalPages}
+					currentNum={page + 1}
+				/>
+			</PaginationRootDiv>
 			<ReplyAdd />
 		</ArticleRootMain>
 	);
